5165dc02b64b7489ca85c89d9334ebbbecca4d21
jamesball27 <jwrb27@gmail.com>
2020-04-04 18:58:14 +0100

    Setup graphql server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,32 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
