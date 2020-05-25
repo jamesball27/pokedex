7d0890911e55c198f9d7da92dbca532c0374daad
jamesball27 <jwrb27@gmail.com>
2020-05-25 16:41:29 +0100

    Setup basic pokemon list with infinite scroll

--- client/components/Pokedex.tsx
+++ client/components/Pokedex.tsx
@@ -1,39 +1,16 @@
 import React from 'react';
-import { gql } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
+
 import { Layout } from 'antd';
 
 import PokemonList from './PokemonList';
 
 const { Sider, Content } = Layout;
 
-const POKEDEX_QUERY = gql`
-  {
-    pokedex {
-      pokemonEntries {
-        species {
-          id
-          name
-        }
-      }
-    }
-  }
-`;
-
-const Pokedex = () => {
-  const { loading, error, data } = useQuery(POKEDEX_QUERY);
-
-  if (loading) {
-    return <h1>Loading...</h1>;
-  }
-  if (error) {
-    return <h1>ERROR</h1>;
-  }
-
+const Pokedex: React.FC = () => {
   return (
     <Layout style={{ height: '100%' }}>
-      <Sider width="40%">
-        <PokemonList pokemon={data.pokedex.pokemonEntries} />
+      <Sider width="40%" theme="light" style={{ height: '100%', overflow: 'scroll' }}>
+        <PokemonList />
       </Sider>
       <Content>Content</Content>
     </Layout>
--- client/components/PokemonList.tsx
+++ client/components/PokemonList.tsx
@@ -1,21 +1,91 @@
-import React from 'react';
-import { Menu } from 'antd';
+import React, { useState, useRef } from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+import { List, Spin, BackTop, Button } from 'antd';
+import InfiniteScroll from 'react-infinite-scroller';
 
-import PokemonSpecies from '../../types/PokemonSpecies';
+import Pokedex from '../../types/Pokedex';
 
-interface Props {
-  pokemon: {
-    species: PokemonSpecies;
-  }[];
+interface Query {
+  pokedex: Pokedex;
 }
 
-const PokemonList: React.FC<Props> = ({ pokemon }) => {
+const POKEDEX_QUERY = gql`
+  query($skip: Int!) {
+    pokedex {
+      pokemonEntries(skip: $skip) {
+        species {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+const PokemonList: React.FC = () => {
+  const { loading, error, data, fetchMore } = useQuery<Query>(POKEDEX_QUERY, {
+    variables: {
+      skip: 0,
+    },
+  });
+  const [hasMore, setHasMore] = useState(true);
+  const parentRef = useRef(null);
+
+  if (loading) {
+    return <Spin />;
+  }
+  if (error) {
+    return <h1>ERROR</h1>;
+  }
+
   return (
-    <Menu theme="dark">
-      {pokemon.map(({ species }) => (
-        <Menu.Item key={species.id}>{species.name}</Menu.Item>
-      ))}
-    </Menu>
+    <div style={{ overflow: 'auto', height: '100%' }} ref={parentRef}>
+      <InfiniteScroll
+        initialLoad={false}
+        pageStart={1}
+        loadMore={() => {
+          fetchMore({
+            variables: {
+              skip: data?.pokedex.pokemonEntries.length,
+            },
+            updateQuery: (prev, { fetchMoreResult }) => {
+              if (!fetchMoreResult || fetchMoreResult.pokedex.pokemonEntries.length === 0) {
+                setHasMore(false);
+                return prev;
+              }
+
+              return {
+                pokedex: {
+                  ...prev.pokedex,
+                  pokemonEntries: [
+                    ...prev.pokedex.pokemonEntries,
+                    ...fetchMoreResult.pokedex.pokemonEntries,
+                  ],
+                },
+              };
+            },
+          });
+        }}
+        hasMore={!loading && hasMore}
+        useWindow={false}
+        loader={
+          <List.Item key="loader" style={{ display: 'flex', justifyContent: 'center' }}>
+            <Spin />
+          </List.Item>
+        }
+      >
+        <List
+          dataSource={data?.pokedex.pokemonEntries}
+          size="large"
+          renderItem={(item) => <List.Item key={item.species.id}>{item.species.name}</List.Item>}
+        >
+          <BackTop target={() => parentRef.current || window}>
+            <Button type="primary">Back to Top</Button>
+          </BackTop>
+        </List>
+      </InfiniteScroll>
+    </div>
   );
 };
 
