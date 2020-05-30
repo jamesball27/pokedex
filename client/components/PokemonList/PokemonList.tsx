7c756fe51d646abf96460f829fcc88b33456f9db
jamesball27 <jwrb27@gmail.com>
2020-05-30 21:02:52 +0100

    Refactor components

--- client/components/PokemonList/PokemonList.tsx
+++ client/components/PokemonList/PokemonList.tsx
@@ -1,38 +1,19 @@
 import React, { useState, useRef } from 'react';
-import { gql } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
 import { List, Spin, BackTop, Button, Avatar, Menu, Skeleton } from 'antd';
 import InfiniteScroll from 'react-infinite-scroller';
 import styled from 'styled-components';
 
 import PokemonListItem from './PokemonListItem';
-import Pokedex from '../../types/Pokedex';
-
-interface Query {
-  pokedex: Pokedex;
-}
-
-const POKEDEX_QUERY = gql`
-  query($skip: Int!, $lang: String, $defaultPokemon: Boolean) {
-    pokedex {
-      pokemonEntries(skip: $skip) {
-        entryNumber
-        species {
-          id
-          localeName(lang: $lang)
-          pokemon(default: $defaultPokemon) {
-            images {
-              sprite
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+import PokemonEntry from '../../../types/PokemonEntry';
+import { ApolloError } from 'apollo-boost';
 
 interface Props {
   collapsed: boolean;
+  data?: PokemonEntry[];
+  loadMore: () => void;
+  hasMore: boolean;
+  loading: boolean;
+  error?: ApolloError;
 }
 
 const StyledMenuItem = styled(Menu.Item)`
@@ -43,15 +24,7 @@ const StyledMenuItem = styled(Menu.Item)`
   padding: ${(props: { collapsed: 1 | 0 }) => (props.collapsed ? '0 !important' : 'inherit')};
 `;
 
-const PokemonList: React.FC<Props> = ({ collapsed }) => {
-  const { loading, error, data, fetchMore } = useQuery<Query>(POKEDEX_QUERY, {
-    variables: {
-      skip: 0,
-      lang: 'en',
-      defaultPokemon: true,
-    },
-  });
-  const [hasMore, setHasMore] = useState(true);
+const PokemonList: React.FC<Props> = ({ data, collapsed, loadMore, hasMore, loading, error }) => {
   const parentRef = useRef(null);
 
   if (loading) {
@@ -68,30 +41,6 @@ const PokemonList: React.FC<Props> = ({ collapsed }) => {
     return <h1>ERROR</h1>;
   }
 
-  const loadMore = () => {
-    fetchMore({
-      variables: {
-        skip: data?.pokedex.pokemonEntries.length,
-      },
-      updateQuery: (prev, { fetchMoreResult }) => {
-        if (!fetchMoreResult || fetchMoreResult.pokedex.pokemonEntries.length === 0) {
-          setHasMore(false);
-          return prev;
-        }
-
-        return {
-          pokedex: {
-            ...prev.pokedex,
-            pokemonEntries: [
-              ...prev.pokedex.pokemonEntries,
-              ...fetchMoreResult.pokedex.pokemonEntries,
-            ],
-          },
-        };
-      },
-    });
-  };
-
   return (
     <div style={{ overflow: 'auto', height: '100%' }} ref={parentRef}>
       <InfiniteScroll
@@ -107,7 +56,7 @@ const PokemonList: React.FC<Props> = ({ collapsed }) => {
         }
       >
         <Menu theme="dark">
-          {data?.pokedex.pokemonEntries.map((p) => (
+          {data?.map((p) => (
             <StyledMenuItem
               collapsed={collapsed ? 1 : 0}
               title={p.species.localeName}
